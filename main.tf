349dce4d4095079f2e2bfa61fa60d25a3fa59def
AtharvaWathodkar <111409657+AtharvaWathodkar@users.noreply.github.com>
2024-04-20 13:06:00 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -4,11 +4,24 @@ provider "helm" {
   }
 }
 
+resource "null_resource" "helm_login" {
+  provisioner "local-exec" {
+    command = "gcloud auth print-access-token | helm registry login -u oauth2accesstoken --password-stdin https://us-central1-docker.pkg.dev"
+  }
+
+  triggers = {
+    always_run = "${timestamp()}"
+  }
+}
+
 resource "helm_release" "example" {
   name      = "example"
   namespace = "default"
   chart     = "oci://us-central1-docker.pkg.dev/burner-athwatho/helm-poc/hello-world"
   version   = "0.1.0"
+ depends_on = [
+    null_resource.helm_login
+  ]
 }
 
 
